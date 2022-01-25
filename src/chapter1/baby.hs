aa7cdc241136b82f29ced7bdb6db2b564ffea62e
준규 <vpark45@gmail.com>
2022-01-26 07:38:47 +0900

    Add function removeNonUppercase

--- src/chapter1/baby.hs
+++ src/chapter1/baby.hs
@@ -13,3 +13,5 @@ conanO'Brien = "It's a-me, Conan O'Brien!"
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
 length' xs = sum [1 | _ <- xs]
+
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
