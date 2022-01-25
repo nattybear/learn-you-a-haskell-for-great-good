79de21f2c0e74bed96773106e2cefa45d5d29de8
준규 <vpark45@gmail.com>
2022-01-26 00:34:15 +0900

    Add boomBangs

--- src/chapter1/baby.hs
+++ src/chapter1/baby.hs
@@ -9,3 +9,5 @@ doubleSmallNumber x = if x > 100
 doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
 
 conanO'Brien = "It's a-me, Conan O'Brien!"
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
