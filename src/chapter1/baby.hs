d43c3ecdb22236da5e251224eea1659197360ac0
준규 <vpark45@gmail.com>
2022-01-28 19:16:18 +0900

    Add type declaration

--- src/chapter1/baby.hs
+++ src/chapter1/baby.hs
@@ -14,4 +14,5 @@ boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
 length' xs = sum [1 | _ <- xs]
 
+removeNonUppercase :: [Char] -> [Char]
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
