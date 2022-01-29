7a99ebe5fd72a35d21aa017da4581324fc1b60ae
준규 <vpark45@gmail.com>
2022-01-30 00:06:57 +0900

    Rewrite bmiTell function using where

--- src/chapter3/guards.hs
+++ src/chapter3/guards.hs
@@ -1,9 +1,10 @@
 bmiTell :: Double -> Double -> String
 bmiTell weight height
-  | weight / height ^ 2 <= 18.5 = "You're underweight, eat more!"
-  | weight / height ^ 2 <= 25.0 = "Looking good!"
-  | weight / height ^ 2 <= 30.0 = "You're overweight. Let's work out together!"
+  | bmi <= 18.5 = "You're underweight, eat more!"
+  | bmi <= 25.0 = "Looking good!"
+  | bmi <= 30.0 = "You're overweight. Let's work out together!"
   | otherwise = "You're obese. Go see a doctor."
+  where bmi = weight / height ^ 2
 
 max' :: (Ord a) => a -> a -> a
 max' a b
