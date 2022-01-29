5dd3b6281f4d23f40fee92036777af66ca1d3dc8
준규 <vpark45@gmail.com>
2022-01-30 00:01:56 +0900

    Modify bmiTell to take a height and weight

--- src/chapter3/guards.hs
+++ src/chapter3/guards.hs
@@ -1,6 +1,6 @@
-bmiTell :: Double -> String
-bmiTell bmi
-  | bmi <= 18.5 = "You're underweight, eat more!"
-  | bmi <= 25.0 = "Looking good!"
-  | bmi <= 30.0 = "You're overweight. Let's work out together!"
+bmiTell :: Double -> Double -> String
+bmiTell weight height
+  | weight / height ^ 2 <= 18.5 = "You're underweight, eat more!"
+  | weight / height ^ 2 <= 25.0 = "Looking good!"
+  | weight / height ^ 2 <= 30.0 = "You're overweight. Let's work out together!"
   | otherwise = "You're obese. Go see a doctor."
