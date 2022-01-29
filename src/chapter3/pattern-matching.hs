83593a3405ecd1ef363b35dbf0f626218cb9e231
준규 <vpark45@gmail.com>
2022-01-29 23:49:46 +0900

    Add badAdd function

--- src/chapter3/pattern-matching.hs
+++ src/chapter3/pattern-matching.hs
@@ -41,3 +41,6 @@ tell (x:[]) = "The list has one element: " ++ show x
 tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
 tell (x:y:_) = "This list is long. The first two elements are: " ++ show x
                ++ " and " ++ show y
+
+badAdd :: (Num a) => [a] -> a
+badAdd (x:y:z:[]) = x + y + z
