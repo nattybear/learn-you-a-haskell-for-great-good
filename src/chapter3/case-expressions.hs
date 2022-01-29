1a687b1036cddbb25981f017faa8fa2bd78a7921
준규 <vpark45@gmail.com>
2022-01-30 00:43:12 +0900

    Rewrite describeList function using where

--- src/chapter3/case-expressions.hs
+++ src/chapter3/case-expressions.hs
@@ -1,4 +1,5 @@
 describeList :: [a] -> String
-describeList ls = "The list is " ++ case ls of []  -> "empty."
-                                               [x] -> "a singleton list."
-                                               xs  -> "a longer list."
+describeList ls = "The list is " ++ what ls
+  where what [] = "empty."
+        what [x] = "a singleton list."
+        what xs = "a longer list."
