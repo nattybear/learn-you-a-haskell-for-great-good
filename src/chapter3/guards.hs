c622093af1b2f225b16fd7f73262e109033943d9
준규 <vpark45@gmail.com>
2022-01-30 00:03:33 +0900

    Implement our own max function

--- src/chapter3/guards.hs
+++ src/chapter3/guards.hs
@@ -4,3 +4,8 @@ bmiTell weight height
   | weight / height ^ 2 <= 25.0 = "Looking good!"
   | weight / height ^ 2 <= 30.0 = "You're overweight. Let's work out together!"
   | otherwise = "You're obese. Go see a doctor."
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+  | a <= b = b
+  | otherwise = a
