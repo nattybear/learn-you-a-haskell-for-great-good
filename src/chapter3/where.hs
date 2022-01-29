af65dc13fe05c07fbbdde7f119bc1756f24755a5
준규 <vpark45@gmail.com>
2022-01-30 00:15:07 +0900

    Fix typo

--- src/chapter3/where.hs
+++ src/chapter3/where.hs
@@ -7,4 +7,4 @@ niceGreeting = "Hello! So very nice to see you,"
 greet :: String -> String
 greet "Juan" = niceGreeting ++ " Juan!"
 greet "Fernando" = niceGreeting ++ " Fernando!"
-greet name = badGreeting ++ " " + name
+greet name = badGreeting ++ " " ++ name
