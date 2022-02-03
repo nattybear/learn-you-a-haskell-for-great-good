023c9f27f4d2d77088edb7d79961526a17b03830
준규 <vpark45@gmail.com>
2022-02-03 22:10:35 +0900

    Rewrite numLongChains function using lambda

--- src/chapter5/toolbox.hs
+++ src/chapter5/toolbox.hs
@@ -19,5 +19,4 @@ chain n
   | odd n = n:chain (n*3 + 1)
 
 numLongChains :: Int
-numLongChains = length (filter isLong (map chain [1..100]))
-  where isLong xs = length xs > 15
+numLongChains = length (filter (\xs -> length xs > 15) (map chain [1..100]))
