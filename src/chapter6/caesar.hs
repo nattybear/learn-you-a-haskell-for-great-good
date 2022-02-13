51962256d2ce603c41f9842d4f8f6517568fb77f
준규 <vpark45@gmail.com>
2022-02-13 20:46:18 +0900

    Add decode function

--- src/chapter6/caesar.hs
+++ src/chapter6/caesar.hs
@@ -2,3 +2,6 @@ import Data.Char
 
 encode :: Int -> String -> String
 encode offset msg = map (chr . (+ offset) . ord) msg
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
