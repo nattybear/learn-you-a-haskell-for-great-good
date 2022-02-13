74f0e7606d2c5ab749e2888b3b99484a14a28537
준규 <vpark45@gmail.com>
2022-02-13 21:45:51 +0900

    Add phoneBookToMap function and add more numbers

--- src/chapter6/phone-book.hs
+++ src/chapter6/phone-book.hs
@@ -1,14 +1,17 @@
 import qualified Data.Map as Map
 import Data.Char
 
-phoneBook :: Map.Map String String
-phoneBook = Map.fromList $
+phoneBook =
   [("betty", "555-2938")
+  ,("betty", "342-2492")
   ,("bonnie", "452-2928")
   ,("patsy", "493-2938")
+  ,("patsy", "943-2929")
+  ,("patsy", "827-9162")
   ,("lucille", "205-2928")
   ,("wendy", "939-8282")
   ,("penny", "853-2492")
+  ,("penny", "555-2111")
   ]
 
 findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
@@ -17,3 +20,7 @@ findKey key xs =
 
 string2digits :: String -> [Int]
 string2digits = map digitToInt . filter isDigit
+
+phoneBookToMap :: (Ord k) => [(k, String)] -> Map.Map k String
+phoneBookToMap xs = Map.fromListWith add xs
+  where add number1 number2 = number1 ++ ", " ++ number2
