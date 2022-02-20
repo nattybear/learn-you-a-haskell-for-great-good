6a8fccc4d906372458c62dbb9f7f2441c540793e
준규 <vpark45@gmail.com>
2022-02-20 16:30:47 +0900

    Rewrite Person type using record syntax

--- src/chapter7/Person.hs
+++ src/chapter7/Person.hs
@@ -1,2 +1,6 @@
-data Person = Person String String Int Float String String
-  deriving (Show)
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavor :: String } deriving (Show)
