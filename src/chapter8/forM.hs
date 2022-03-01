06ba2f64ad98d4a72a8fca9c8ff517aff0a2485a
준규 <vpark45@gmail.com>
2022-03-01 15:54:28 +0900

    Rewrite without return

--- src/chapter8/forM.hs
+++ src/chapter8/forM.hs
@@ -4,7 +4,6 @@ main = do
   colors <- forM [1,2,3,4] (\a -> do
     putStrLn $ "Which color do you associate with the number "
                ++ show a ++ "?"
-    color <- getLine
-    return color)
+    getLine)
   putStrLn "The colors that you associate with 1, 2, 3 and 4 are: "
   mapM putStrLn colors
