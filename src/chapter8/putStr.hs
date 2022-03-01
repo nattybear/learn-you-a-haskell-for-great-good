f6248c0d0d171ce8a291179371e63c7e21b25033
준규 <vpark45@gmail.com>
2022-03-01 15:13:02 +0900

    Implement putStr function using putChar

--- src/chapter8/putStr.hs
+++ src/chapter8/putStr.hs
@@ -1,4 +1,10 @@
+putStr' :: String -> IO ()
+putStr' [] = return ()
+putStr' (x:xs) = do
+  putChar x
+  putStr' xs
+
 main = do
-  putStr "Hey, "
-  putStr "I'm "
+  putStr' "Hey, "
+  putStr' "I'm "
   putStrLn "Andy!"
