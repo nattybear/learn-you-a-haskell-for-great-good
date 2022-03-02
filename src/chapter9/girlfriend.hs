98f291b32feefbe8bbed67acf161024ee689fa32
준규 <vpark45@gmail.com>
2022-03-02 20:53:38 +0900

    Rewrite using withFile

--- src/chapter9/girlfriend.hs
+++ src/chapter9/girlfriend.hs
@@ -1,7 +1,6 @@
 import System.IO
 
 main = do
-  handle <- openFile "girlfriend.txt" ReadMode
-  contents <- hGetContents handle
-  putStr contents
-  hClose handle
+  withFile "girlfriend.txt" ReadMode (\handle -> do
+    contents <- hGetContents handle
+    putStr contents)
