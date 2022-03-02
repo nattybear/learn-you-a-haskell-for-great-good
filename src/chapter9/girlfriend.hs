645130f2d159f7ec72dc5c41cddba50a21794e1a
준규 <vpark45@gmail.com>
2022-03-02 21:10:07 +0900

    Rewrite using readFile function

--- src/chapter9/girlfriend.hs
+++ src/chapter9/girlfriend.hs
@@ -1,6 +1,5 @@
 import System.IO
 
 main = do
-  withFile "girlfriend.txt" ReadMode (\handle -> do
-    contents <- hGetContents handle
-    putStr contents)
+  contents <- readFile "girlfriend.txt"
+  putStr contents
