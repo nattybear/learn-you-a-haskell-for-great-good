798af844d8d3c5f9b7655c2f0b9ddb5c42a8a9ef
준규 <vpark45@gmail.com>
2022-03-05 01:14:01 +0900

    Rewrite to do all the work in main

--- src/chapter9/guess_the_number.hs
+++ src/chapter9/guess_the_number.hs
@@ -3,19 +3,13 @@ import Control.Monad(when)
 
 main = do
   gen <- getStdGen
-  askForNumber gen
-
-askForNumber :: StdGen -> IO ()
-askForNumber gen = do
-  let (randNumber, newGen) = randomR (1,10) gen :: (Int, StdGen)
+  let (randNumber, _) = randomR (1,10) gen :: (Int, StdGen)
   putStrLn "Which number in the range from 1 to 10 am I thinking of? "
   numberString <- getLine
   when (not $ null numberString) $ do
-    let xs = reads numberString
-        number = if null xs
-                   then (-1)
-                   else fst $ head xs
+    let number = read numberString
     if randNumber == number
       then putStrLn "You are correct!"
       else putStrLn $ "Sorry, it was " ++ show randNumber
-    askForNumber newGen
+    newStdGen
+    main
