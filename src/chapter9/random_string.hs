83f3385223c298ad076ba6db05737a90816e5d5f
준규 <vpark45@gmail.com>
2022-03-05 00:10:51 +0900

    newStdGen updates the global random generator

--- src/chapter9/random_string.hs
+++ src/chapter9/random_string.hs
@@ -3,5 +3,5 @@ import System.Random
 main = do
   gen <- getStdGen
   putStrLn $ take 20 (randomRs ('a', 'z') gen)
-  gen2 <- getStdGen
-  putStrLn $ take 20 (randomRs ('a', 'z') gen2)
+  gen' <- newStdGen
+  putStrLn $ take 20 (randomRs ('a', 'z') gen')
