69cee07aee01c74aa2b80e50ac5d83b838721695
준규 <vpark45@gmail.com>
2022-03-30 14:08:30 +0900

    Rewrite sequenceA using foldr

--- src/chapter11/sequenceA.hs
+++ src/chapter11/sequenceA.hs
@@ -1,3 +1,4 @@
+import Control.Applicative
+
 sequenceA' :: (Applicative f) => [f a] -> f [a]
-sequenceA' [] = pure []
-sequenceA' (x:xs) = (:) <$> x <*> sequenceA' xs
+sequenceA' = foldr (liftA2 (:)) (pure [])
