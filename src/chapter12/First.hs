63471b9d8e91d8d35792b562515fe124dccaca58
준규 <vpark45@gmail.com>
2022-03-31 20:21:32 +0900

    Implement Monoid instance for First new type

--- src/chapter12/First.hs
+++ src/chapter12/First.hs
@@ -1,2 +1,7 @@
 newtype First a = First { getFirst :: Maybe a }
   deriving (Eq, Ord, Read, Show)
+
+instance Monoid (First a) where
+  mempty = First Nothing
+  First (Just x) `mappend` _ = First (Just x)
+  First Nothing `mappend` x = x
