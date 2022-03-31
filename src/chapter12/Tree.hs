2f6f459737d3cc62322d514ea2b87976e4f3daf1
준규 <vpark45@gmail.com>
2022-03-31 21:06:27 +0900

    Add testTree

--- src/chapter12/Tree.hs
+++ src/chapter12/Tree.hs
@@ -11,3 +11,14 @@ instance F.Foldable Tree where
   foldMap f (Node x l r) = F.foldMap f l `mappend`
                            f x           `mappend`
                            F.foldMap f r
+
+testTree :: Tree Int
+testTree = Node 5
+             (Node 3
+               (Node 1 EmptyTree EmptyTree)
+               (Node 6 EmptyTree EmptyTree)
+             )
+             (Node 9
+               (Node 8 EmptyTree EmptyTree)
+               (Node 10 EmptyTree EmptyTree)
+             )
