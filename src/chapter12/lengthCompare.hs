c899eeda6e064a7d0f826f79d96336920fa0edeb
준규 <vpark45@gmail.com>
2022-03-31 20:03:45 +0900

    Add lengthCompare function

---
+++ src/chapter12/lengthCompare.hs
@@ -0,0 +1,4 @@
+lengthCompare :: String -> String -> Ordering
+lengthCompare x y = let a = length x `compare` length y
+                        b = x `compare` y
+                    in  if a == EQ then b else a
