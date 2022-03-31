f724603ea5eb9b8cc98fd062957c2ba05130ea37
준규 <vpark45@gmail.com>
2022-03-31 20:54:24 +0900

    Implement Functor instance for Tree data type

--- src/chapter12/Tree.hs
+++ src/chapter12/Tree.hs
@@ -1,1 +1,5 @@
 data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+instance Functor Tree where
+  fmap _ EmptyTree = EmptyTree
+  fmap f (Node x left right) = Node (f x) (f <$> left) (f <$> right)
