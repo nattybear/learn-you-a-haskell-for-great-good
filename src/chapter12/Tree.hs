b4a0bc6a237e7bdaa01c1119d9c4ddafce887c1c
준규 <vpark45@gmail.com>
2022-03-31 21:00:44 +0900

    Implement Foldable instance for Tree

--- src/chapter12/Tree.hs
+++ src/chapter12/Tree.hs
@@ -1,5 +1,13 @@
+import qualified Data.Foldable as F
+
 data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
 
 instance Functor Tree where
   fmap _ EmptyTree = EmptyTree
   fmap f (Node x left right) = Node (f x) (f <$> left) (f <$> right)
+
+instance F.Foldable Tree where
+  foldMap _ EmptyTree = mempty
+  foldMap f (Node x l r) = F.foldMap f l `mappend`
+                           f x           `mappend`
+                           F.foldMap f r
