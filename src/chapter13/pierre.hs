ad4f99d9cd19d6fe6fefcbf1ef53574a5d87a8d4
준규 <vpark45@gmail.com>
2022-04-01 13:18:23 +0900

    Rewrite land functions using Maybe

--- src/chapter13/pierre.hs
+++ src/chapter13/pierre.hs
@@ -1,11 +1,15 @@
 type Birds = Int
 type Pole = (Birds, Birds)
 
-landLeft :: Birds -> Pole -> Pole
-landLeft n (left, right) = (left + n, right)
+landLeft :: Birds -> Pole -> Maybe Pole
+landLeft n (left, right)
+  | abs ((left + n) - right) < 4 = Just (left + n, right)
+  | otherwise                    = Nothing
 
-landRight :: Birds -> Pole -> Pole
-landRight n (left, right) = (left, right + n)
+landRight :: Birds -> Pole -> Maybe Pole
+landRight n (left, right)
+  | abs (left - (right + n)) < 4 = Just (left, right + n)
+  | otherwise                   = Nothing
 
 (-:) :: a -> (a -> b) -> b
 x -: f = f x
