5685e9c823c582315969f4b1186cb6b7ee45a39b
준규 <vpark45@gmail.com>
2022-04-04 07:36:32 +0900

    Change the type of applyLog

--- src/chapter14/gang.hs
+++ src/chapter14/gang.hs
@@ -1,6 +1,6 @@
 isBigGang :: Int -> (Bool, String)
 isBigGang x = (x > 9, "Compared gang size to 9.")
 
-applyLog :: (a, String) -> (a -> (b, String)) -> (b, String)
+applyLog :: (Monoid m) => (a, m) -> (a -> (b, m)) -> (b, m)
 applyLog (x, log) f = let (y, newLog) = f x
-                      in  (y, log ++ newLog)
+                      in  (y, log `mappend` newLog)
