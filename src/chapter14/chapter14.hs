d8f42aaf2b442ed17eca8b913680d9ffbd92216f
준규 <vpark45@gmail.com>
2022-04-04 13:49:11 +0900

    Rewrite gcd function using Writer Monad

--- src/chapter14/chapter14.hs
+++ src/chapter14/chapter14.hs
@@ -25,3 +25,12 @@ multWithLog  = do
   b <- logNumber 5
   tell ["Gonna multiply these two"]
   return (a * b)
+
+gcd' :: Int -> Int -> Writer [String] Int
+gcd' a b
+  | b == 0 = do
+      tell ["Finished with " ++ show a]
+      return a
+  | otherwise = do
+      tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+      gcd' b (a `mod` b)
