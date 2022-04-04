15c405618733dd000bf1e4f2495ef71ee09d6d1a
준규 <vpark45@gmail.com>
2022-04-04 12:54:45 +0900

    Add multWithLog monadic value

--- src/chapter14/chapter14.hs
+++ src/chapter14/chapter14.hs
@@ -1,3 +1,4 @@
+import Control.Monad.Writer
 import Data.Monoid
 
 isBigGang :: Int -> (Bool, String)
@@ -14,3 +15,12 @@ addDrink :: Food -> (Food, Price)
 addDrink "beans" = ("milk", Sum 25)
 addDrink "jerky" = ("whiskey", Sum 99)
 addDrink _ = ("beer", Sum 30)
+
+logNumber :: Int -> Writer [String] Int
+logNumber x = writer (x, ["Got number: " ++ show x])
+
+multWithLog :: Writer [String] Int
+multWithLog  = do
+  a <- logNumber 3
+  b <- logNumber 5
+  return (a * b)
