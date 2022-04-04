59f69d1578f8ff996d807f298546c698d8cda74d
준규 <vpark45@gmail.com>
2022-04-05 00:48:20 +0900

    Rewrite gcdReverse using DiffList

--- src/chapter14/chapter14.hs
+++ src/chapter14/chapter14.hs
@@ -35,14 +35,14 @@ gcd' a b
       tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
       gcd' b (a `mod` b)
 
-gcdReverse :: Int -> Int -> Writer [String] Int
+gcdReverse :: Int -> Int -> Writer (DiffList String) Int
 gcdReverse a b
   | b == 0 = do
-      tell ["Finished with " ++ show a]
+      tell (toDiffList ["Finished with " ++ show a])
       return a
   | otherwise = do
       result <- gcdReverse b (a `mod` b)
-      tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+      tell (toDiffList [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)])
       return result
 
 newtype DiffList a = DiffList { getDiffList :: [a] -> [a] }
