206be1d4ecdcf9ea76b23f83a8a2cb6b196fe112
준규 <vpark45@gmail.com>
2022-04-06 12:29:25 +0900

    Add solveRPN function

--- src/chapter14/rpn.hs
+++ src/chapter14/rpn.hs
@@ -1,6 +1,11 @@
 import Data.List
 import Control.Monad
 
+solveRPN :: String -> Maybe Double
+solveRPN st = do
+  [result] <- foldM foldingFunction [] (words st)
+  return result
+
 foldingFunction :: [Double] -> String -> Maybe [Double]
 foldingFunction (x:y:ys) "*" = return ((y * x):ys)
 foldingFunction (x:y:ys) "+" = return ((y + x):ys)
