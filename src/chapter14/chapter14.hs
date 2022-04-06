973a11eff05999a165be6badf1fae786d5cc9c1f
준규 <vpark45@gmail.com>
2022-04-06 10:55:22 +0900

    Add keepSmall function

--- src/chapter14/chapter14.hs
+++ src/chapter14/chapter14.hs
@@ -75,3 +75,12 @@ joinedMaybe :: Maybe Int
 joinedMaybe = do
   m <- Just (Just 8)
   m
+
+keepSmall :: Int -> Writer [String] Bool
+keepSmall x
+  | x < 4 = do
+      tell ["Keeping " ++ show 4]
+      return True
+  | otherwise = do
+      tell [show x ++ " is too large, throwing it away"]
+      return False
