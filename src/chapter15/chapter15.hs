dc327e74ef6cb2778125fe13708824b13f695f58
준규 <vpark45@gmail.com>
2022-04-12 12:02:15 +0900

    Add elemAt function

--- src/chapter15/chapter15.hs
+++ src/chapter15/chapter15.hs
@@ -31,3 +31,8 @@ changeToP :: Directions -> Tree Char -> Tree Char
 changeToP (L:ds) (Node x l r) = Node x (changeToP ds l) r
 changeToP (R:ds) (Node x l r) = Node x l (changeToP ds r)
 changeToP [] (Node _ l r) = Node 'P' l r
+
+elemAt :: Directions -> Tree a -> a
+elemAt (L:ds) (Node _ l _) = elemAt ds l
+elemAt (R:ds) (Node _ _ r) = elemAt ds r
+elemAt [] (Node x _ _) = x
