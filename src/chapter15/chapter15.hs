b201ac54cdce6142d0242ca495aa0d9b36b77a4c
준규 <vpark45@gmail.com>
2022-04-12 11:58:49 +0900

    Rewrite changeToP using Direction

--- src/chapter15/chapter15.hs
+++ src/chapter15/chapter15.hs
@@ -24,5 +24,10 @@ freeTree =
       )
     )
 
-changeToP :: Tree Char -> Tree Char
-changeToP (Node x l (Node y (Node _ m n) r)) = Node x l (Node y (Node 'P' m n) r)
+data Direction = L | R deriving (Show)
+type Directions = [Direction]
+
+changeToP :: Directions -> Tree Char -> Tree Char
+changeToP (L:ds) (Node x l r) = Node x (changeToP ds l) r
+changeToP (R:ds) (Node x l r) = Node x l (changeToP ds r)
+changeToP [] (Node _ l r) = Node 'P' l r
