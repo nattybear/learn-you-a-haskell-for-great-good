ddc8a2b634064bd865fc25be1b67cb92e6984ded
준규 <vpark45@gmail.com>
2022-04-12 13:28:25 +0900

    Add attach function

--- src/chapter15/chapter15.hs
+++ src/chapter15/chapter15.hs
@@ -59,3 +59,6 @@ type Zipper a = (Tree a, Breadcrumbs a)
 modify :: (a -> a) -> Zipper a -> Zipper a
 modify f (Node x l r, bs) = (Node (f x) l r, bs)
 modify f (Empty, bs) = (Empty, bs)
+
+attach :: Tree a -> Zipper a -> Zipper a
+attach t (_, bs) = (t, bs)
